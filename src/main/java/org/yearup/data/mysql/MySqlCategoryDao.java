e72f2ad840f74bf867488311aaae5cc0856013d1
David-Aldair <d.aldair.quinonez@gmail.com>
2025-12-16 21:50:33 -0500

    Implement getAllCategories and getById with logging

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,32 +5,102 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Component
-public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
-{
+public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao{
+
+    //a logger records what your application is doing especially when something goes wrong
+    //so you can debug and monitor it without guessing
+    private Logger logger = Logger.getLogger(MySqlCategoryDao.class.getName());
+
     public MySqlCategoryDao(DataSource dataSource)
     {
         super(dataSource);
     }
 
+    //method to get all categories
     @Override
-    public List<Category> getAllCategories()
-    {
-        // get all categories
-        return null;
+
+    //creating a list
+    public List<Category> getAllCategories() {
+
+        //list to hold all category objects retrieved from the database
+        List<Category> categories = new ArrayList<>();
+
+        //try-with-resources to manage connection, prepared statement, and result set
+        try (Connection connection = dataSource.getConnection()) {
+
+            PreparedStatement statement = connection.prepareStatement("""
+                    SELECT
+                        category_id,
+                        name,
+                        description
+                    FROM
+                        categories
+                    """);
+            ResultSet resultSet = statement.executeQuery();
+            {
+                //loop through each row in the resultSet
+                while (resultSet.next()) {
+                    Category category = new Category();
+                    category.setCategoryId(resultSet.getInt("category_id"));
+                    category.setName(resultSet.getString("name"));
+                    category.setDescription(resultSet.getString("description"));
+                    categories.add(category);
+                }
+            }
+        } catch (SQLException e) {
+            //log any exceptions with full stack trace
+            logger.log(Level.SEVERE, "Error getting all the categories", e);
+        }
+        //return list of categories
+        return categories;
     }
 
+    //method to get categories by ID
     @Override
-    public Category getById(int categoryId)
-    {
-        // get category by id
-        return null;
+    public Category getById(int categoryId) {
+    Category category = new Category();
+
+    try (Connection connection = dataSource.getConnection();
+
+         PreparedStatement stmt = connection.prepareStatement(
+                 """
+                        SELECT
+                            category_id,
+                            name,
+                            description
+                        FROM
+                            categories
+                        WHERE category_id = ?
+                        """)) {
+
+        stmt.setInt(1, categoryId);
+
+        //nested try to ensure the result set is closed properly
+        try (ResultSet resultSet = stmt.executeQuery()) {
+            if (resultSet.next()) {
+                category.setCategoryId(resultSet.getInt("category_id"));
+                category.setName(resultSet.getString("name"));
+                category.setDescription(resultSet.getString("description"));
+            }
+        }
+    } catch (SQLException e) {
+        logger.log(Level.SEVERE, "Error getting category by ID: " + categoryId, e);
+    }
+        return category;
     }
 
+
+    //method to create a category
     @Override
     public Category create(Category category)
     {
@@ -38,12 +108,14 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
         return null;
     }
 
+    //method to update a category
     @Override
     public void update(int categoryId, Category category)
     {
         // update category
     }
 
+    //method to delete a category
     @Override
     public void delete(int categoryId)
     {
