7883db49f87d261b85636e12d69412acfe4497a9
David-Aldair <d.aldair.quinonez@gmail.com>
2025-12-17 22:09:20 -0500

    Add ShoppingCartDao method signatures

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -6,4 +6,13 @@ public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
     // add additional method signatures here
+
+    // Adds a product or increases quantity by 1
+    void addToCart(int productId, int userId);
+
+    // Updates quantity of an existing cart item
+    void editCart(int productId, int userId, int quantity);
+
+    // Clears the user's shopping cart
+    void clearCart(int userId);
 }
